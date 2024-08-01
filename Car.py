9064d9fc07cb41f56971df4cec1a3d7b738fea00
Usama Ali <usamaaliawan88@gmail.com>
2024-08-01 13:55:15 +0500

    added more methods

--- Car.py
+++ Car.py
@@ -1,13 +1,15 @@
 import json
 from vehical import Vehicle
 class car(Vehicle):
-    def __init__(self,model,company,lisence_plate,rent_price,car_name):
+    def __init__(self,model,company,lisence_plate,rent_price,car_name,car_type):
         super().__init__(model,company,lisence_plate,rent_price)
         self.car_name=car_name
+        self.car_type=car_type
 
     def add_vehicle(self):
 
         car_info={"Car Name":self.car_name,
+                  "Car "
                    "Model":self.model,
                     "Company":self.company,
                      "plate no":self.lisence_plate,
@@ -34,6 +36,24 @@ class car(Vehicle):
                 print(f"Plate No: {carz['plate no']}")
                 print(f"Rent per day {carz['Rent per day']}\n")
 
+    def search_vehicel(self):
+        with open("/Users/apple/Documents/Vehical-rental-system/cars.json",'r') as fileobj:
+            name=input("Enter the car name you want to search from collection")
+            CARS=json.load(fileobj)
+            for carz in CARS:
+                if name==carz["Car Name"]:
+
+                  print(f"Car Name: {carz["Car Name"]}")
+                  print(f"Model: {carz["Model"]}")
+                  print(f"Company: {carz["Company"]}")
+                  print(f"Plate No: {carz['plate no']}")
+                  print(f"Rent per day {carz['Rent per day']}\n")
+
+
+
+
+
+    
 
 
 
