e85525b892f9fee413acaac8c09cb32b3b786d27
Usama Ali <usamaaliawan88@gmail.com>
2024-08-02 01:05:21 +0500

    made the vehical class and abstract class

--- Car.py
+++ Car.py
@@ -1,24 +1,31 @@
 import json
 from vehical import Vehicle
 class car(Vehicle):
-    def __init__(self,model,company,lisence_plate,rent_price,car_name,car_type):
-        super().__init__(model,company,lisence_plate,rent_price)
+    def __init__(self,model,company,lisence_plate,rent_price,avaiable,car_name,car_type):
+        super().__init__(model,company,lisence_plate,rent_price,avaiable)
         self.car_name=car_name
         self.car_type=car_type
 
     def add_vehicle(self):
 
         car_info={"Car Name":self.car_name,
-                  "Car "
+                  "Car Type":self.car_type,
+                  "Availability":self.available,
                    "Model":self.model,
                     "Company":self.company,
                      "plate no":self.lisence_plate,
                      "Rent per day":self.rent_price}
         car_data=[]
-        with open("/Users/apple/Documents/Vehical-rental-system/cars.json", 'r') as fileobj:
-            car_data=json.load(fileobj)
+        with open("/Users/apple/Documents/Vehical-rental-system/cars.json", 'a+') as fileobj:
+            fileobj.seek(0)
+            try:
 
-        car_data.append(car_info)
+              car_data=json.load(fileobj)
+
+            except json.JSONDecodeError:
+                car_data=[]
+
+            car_data.append(car_info)
 
         with open("/Users/apple/Documents/Vehical-rental-system/cars.json",'w') as fileobj:
             json.dump(car_data,fileobj,indent=4)
@@ -27,35 +34,66 @@ class car(Vehicle):
     def show_all_vehicels(self):
 
         with open("/Users/apple/Documents/Vehical-rental-system/cars.json","r") as fileobj:
-            CARS=json.load(fileobj)
-            for carz in CARS:
+            try:
 
-                print(f"Car Name: {carz["Car Name"]}")
-                print(f"Model: {carz["Model"]}")
-                print(f"Company: {carz["Company"]}")
-                print(f"Plate No: {carz['plate no']}")
-                print(f"Rent per day {carz['Rent per day']}\n")
+              CARS=json.load(fileobj)
+              print("ALL CARS:\n")
+              for carz in CARS:
+                 print(carz['Car Name'].upper() + "'s INFO")
+                 print(f"Car Name: {carz["Car Name"].upper()}")
+                 print(f"Car type: {carz['Car Type']}")
+                 print(f"Availability: {carz['Availability']}")
+                 print(f"Model: {carz["Model"]}")
+                 print(f"Company: {carz["Company"]}")
+                 print(f"Plate No: {carz['plate no']}")
+                 print(f"Rent per day {carz['Rent per day']}\n")
+                 print("_____________________________________")
+
+            except  json.JSONDecodeError:
+                print("FILE contain nothing")
 
     def search_vehicel(self):
         with open("/Users/apple/Documents/Vehical-rental-system/cars.json",'r') as fileobj:
-            name=input("Enter the car name you want to search from collection")
-            CARS=json.load(fileobj)
-            for carz in CARS:
-                if name==carz["Car Name"]:
-
-                  print(f"Car Name: {carz["Car Name"]}")
-                  print(f"Model: {carz["Model"]}")
-                  print(f"Company: {carz["Company"]}")
-                  print(f"Plate No: {carz['plate no']}")
-                  print(f"Rent per day {carz['Rent per day']}\n")
+          name=input("Enter the car name you want to search from collection")
+          try:
 
 
+              CARS=json.load(fileobj)
+              for carz in CARS:
+                 if name==carz["Car Name"]:
+
+                   print(f"Car Name: {carz["Car Name"]}")
+                   print(f"Car Type: {carz['Car Type']}")
+                   print(f"Availability: {carz['Availability']}")
+                   print(f"Model: {carz["Model"]}")
+                   print(f"Company: {carz["Company"]}")
+                   print(f"Plate No: {carz['plate no']}")
+                   print(f"Rent per day {carz['Rent per day']}\n")
+                 else:
+
+                    print("CAR NOT FOUND")
+
+          except json.JSONDecodeError:
+
+              print("CAR NOT AVAILABLE")
 
 
 
-    
 
+    def check_availability(self,name,plate):
 
+        with open("/Users/apple/Documents/Vehical-rental-system/cars.json",'r') as fileobj:
+
+            try:
+                CARS=json.load(fileobj)
+                for carz in CARS:
+                    if carz['Car Name'].lower()==name.lower() and carz["plate no"].lower()==plate.lower():
+                        if carz["Availability"].lower()=='yes':
+                            return True
+                return False
+
+            except json.JSONDecodeError:
+                print("Car not available")
 
 
 
--- vehical.py
+++ vehical.py
@@ -1,20 +1,28 @@
-import json
-import os
-class Vehicle:
-    def __init__(self,model,company,lisence_plate,rent_price):
+from abc import ABC,abstractmethod
+class Vehicle(ABC):
+    def __init__(self,model,company,lisence_plate,rent_price,available):
         self.model=model
         self.company=company
         self.lisence_plate=lisence_plate
         self.rent_price=rent_price
+        self.available=available
+    @abstractmethod
     def add_vehicle(self):
         raise NotImplementedError
+    @abstractmethod
     def show_all_vehicels(self):
         raise NotImplementedError
 
+    @abstractmethod
     def search_vehicel(self):
         raise NotImplementedError
 
+    @abstractmethod
+    def check_availability(self,name,plate):
+        raise NotImplementedError
 
+
+    @abstractmethod
     def rent_car(self):
         raise NotImplementedError
 
