5fff6bfa8cdf28f7dde20a7f53d1747d736e7f53
Usama Ali <usamaaliawan88@gmail.com>
2024-08-04 20:33:01 +0500

    just checking

--- ADMIN.py
+++ ADMIN.py
@@ -24,12 +24,12 @@ class Admin:
                 print("RENTED CARS RECORD: \n")
                 for carz in CARS:
 
-                    print(f"CUSTOMER NAME: {carz["CUSTOMER NAME"]}")
-                    print(f"CINC: {carz["CINC"]}")
-                    print(f"CAR NAME: {carz["Car Name"]}")
-                    print(f"CAR LISENCE PLATE NO: {carz["Car Plate"]}")
-                    print(f"DAYS OF RENT: {carz["DAYS"]}")
-                    print(f"RENT PER DAY: {carz["RENT PER DAY"]}")
+                    print(f"CUSTOMER NAME: {carz['CUSTOMER NAME']}")
+                    print(f"CINC: {carz['CINC']}")
+                    print(f"CAR NAME: {carz['Car Name']}")
+                    print(f"CAR LISENCE PLATE NO: {carz['Car Plate']}")
+                    print(f"DAYS OF RENT: {carz['DAYS']}")
+                    print(f"RENT PER DAY: {carz['RENT PER DAY']}")
                     print("-----------------------------------------\n")
 
             except json.JSONDecodeError:
@@ -54,12 +54,12 @@ class Admin:
                 BIKEZ = json.load(fileobj)
                 print("RENTED BIKES RECORDS: \n")
                 for bike in BIKEZ:
-                    print(f"CUSTOMER NAME: {bike["CUSTOMER NAME"]}")
-                    print(f"CINC: {bike["CINC"]}")
-                    print(f"BIKE NAME: {bike["BIKE NAME"]}")
-                    print(f"BIKE LISENCE PLATE NO: {bike["PLATE NO"]}")
-                    print(f"DAYS: {bike["DAYS"]}")
-                    print(f"RENT PER DAY: {bike["RENT PER DAY"]}")
+                    print(f"CUSTOMER NAME: {bike['CUSTOMER NAME']}")
+                    print(f"CINC: {bike['CINC']}")
+                    print(f"BIKE NAME: {bike['BIKE NAME']}")
+                    print(f"BIKE LISENCE PLATE NO: {bike['PLATE NO']}")
+                    print(f"DAYS: {bike['DAYS']}")
+                    print(f"RENT PER DAY: {bike['RENT PER DAY']}")
                     print("---------------------------------------\n")
             except json.JSONDecodeError:
                 print("JSON FILE ERROR IN 'rented bikes' FILE")
